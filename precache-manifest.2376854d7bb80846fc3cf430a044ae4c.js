b21a2a127156ea6cfc2968fd0b8035e6ac43338a
SajanKashi <sajan.kashi@nineleaps.com>
2019-12-07 16:34:37 +0530

    Updates

---
+++ precache-manifest.2376854d7bb80846fc3cf430a044ae4c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fa0911048e6bb57a13a8ed9a2c0515c",
+    "url": "/FoodDeliveryApp/index.html"
+  },
+  {
+    "revision": "1990dc092f14b6aed27b",
+    "url": "/FoodDeliveryApp/static/css/main.58448298.chunk.css"
+  },
+  {
+    "revision": "95d2d124022d8542fb32",
+    "url": "/FoodDeliveryApp/static/js/2.7f5604c3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FoodDeliveryApp/static/js/2.7f5604c3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1990dc092f14b6aed27b",
+    "url": "/FoodDeliveryApp/static/js/main.228abfd4.chunk.js"
+  },
+  {
+    "revision": "06bceecaaad2223903f0",
+    "url": "/FoodDeliveryApp/static/js/runtime-main.cbafd24b.js"
+  },
+  {
+    "revision": "ce3448fd0f14de05a9c9bbc2acafc298",
+    "url": "/FoodDeliveryApp/static/media/bg-img1.ce3448fd.jpg"
+  },
+  {
+    "revision": "b343137acbd1e540e7603eedfff9c296",
+    "url": "/FoodDeliveryApp/static/media/cheeseNachos.b343137a.jpeg"
+  },
+  {
+    "revision": "e72a1b3e506c8b31afe2ebcd28c85145",
+    "url": "/FoodDeliveryApp/static/media/farmhousePizza.e72a1b3e.jpeg"
+  },
+  {
+    "revision": "0ca293121f75aae9543cef93b2606060",
+    "url": "/FoodDeliveryApp/static/media/img1.0ca29312.jpg"
+  },
+  {
+    "revision": "3205debcf707066ecac49d02b9256327",
+    "url": "/FoodDeliveryApp/static/media/margheritaPizza.3205debc.jpeg"
+  },
+  {
+    "revision": "8998cb965057e54efd4ce228ffbb6357",
+    "url": "/FoodDeliveryApp/static/media/paneerCheeseBurger.8998cb96.jpeg"
+  }
+]);
